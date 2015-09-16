d3679028ea861594cc8a5fced25d908361e6beb4
Mikkel Dengsoee <mikkeld@google.com>
2015-09-16 20:01:16 +0100

    Initial Commit

---
+++ Lunchtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lunchtime
+//
+//  Created by Mikkel Dengsøe on 9/16/15.
+//  Copyright (c) 2015 Mikkel Dengsøe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
